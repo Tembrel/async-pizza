a1d3c8affb172e1bb60eaddb2dceba9a908ba14b
Tim Peierls <tim@peierls.net>
2017-11-19 15:02:17 -0500

    Full class names

--- src/pizza/PizzaDemo.java
+++ src/pizza/PizzaDemo.java
@@ -4,11 +4,12 @@ package pizza;
  * Driver for LatchPizza and FuturePizza.
  */
 public class PizzaDemo {
+    
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Demo LatchPizza\n");
+        System.out.println("Demo LatchPizzaBuilder\n");
         LatchPizzaBuilder.main(args);
         
-        System.out.println("\n\nDemo FuturePizza\n");
+        System.out.println("\n\nDemo FuturePizzaBulder\n");
         FuturePizzaBuilder.main(args);
     }
 }
